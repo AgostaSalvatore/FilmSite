b4b0cd0931472c9e04a77ad30fcbc072c72d60ac
AgostaSalvatore <agosta.boolean@gmail.com>
2025-11-02 10:30:27 +0100

    first commit

---
+++ FilmsBack/database/seeders/FilmsSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Film;
+use Faker\Generator as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FilmsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $film               = new Film();
+            $film->title        = $faker->sentence(3);
+            $film->description  = $faker->text(100);
+            $film->poster       = $faker->imageUrl(640, 480, 'movies', true);
+            $film->trailer      = $faker->url();
+            $film->genre        = $faker->word();
+            $film->release_date = $faker->date('Y-m-d');
+            $film->rating       = $faker->numberBetween(1, 10);
+            $film->director     = $faker->name();
+            $film->cast         = $faker->name();
+            $film->save();
+        }
+    }
+}
