6e460a13608bb4ae51ceb02caa291950dc2e8437
AgostaSalvatore <agosta.boolean@gmail.com>
2025-11-19 09:56:56 +0100

    feat: enable API routes and add Laravel Sanctum for authentication
    - Added api.php routes configuration to application bootstrap
    - Added laravel/sanctum ^4.0 package dependency to composer.json for API token authentication

---
+++ FilmsBack/app/Http/Controllers/Api/FilmController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Film;
+use Illuminate\Http\Request;
+
+class FilmController extends Controller
+{
+    public function index()
+    {
+        $films = Film::all();
+        return response()->json([
+            'success' => true,
+            'data'    => $films
+        ]);
+    }
+
+    public function show(Film $film)
+    {
+        $film->load('director', 'genres');
+
+        return response()->json([
+            'success' => true,
+            'data'    => $film
+        ]);
+    }
+}
